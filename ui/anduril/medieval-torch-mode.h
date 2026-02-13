7bfa7a41c2d7c60e9678b804ce2a0edab931781a
siegea1117 <siegea1117@gmail.com>
2026-02-13 03:51:31 -0600

    Update medieval-torch-mode.h

--- ui/anduril/medieval-torch-mode.h
+++ ui/anduril/medieval-torch-mode.h
@@ -1,7 +1,7 @@
 // Medieval Torch Mode
 #pragma once
 
-#ifdef USE_MEDIEVAL_TORCH_MODE
+#if defined(USE_MEDIEVAL_TORCH_MODE) && defined(MODEL_SOFIRN_SP36)
 
 uint8_t medieval_torch_state(Event event, uint16_t arg);
 void medieval_torch_iter(void);
